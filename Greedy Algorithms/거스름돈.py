02a0dfc5c452ebeae9f68d7ed20bc58ba697c278
Hansangjin98 <70688424+Hansangjin98@users.noreply.github.com>
2020-12-11 05:54:01 +0900

    Greedy Algorithms

---
+++ Greedy Algorithms/거스름돈.py
@@ -0,0 +1,10 @@
+# 손님에게 거슬러 줘야 할 돈이 N원일 때, 거슬러 줘야 할 동전의 최소 개수를 구하라
+
+N = int(input("거스름돈을 입력하세요: "))
+count = 0
+coin_type=[500, 100, 50, 10]
+for coin in coin_type:
+    count += N // coin
+    N %= coin
+
+print("거슬러 줘야 할 동전의 개수: " + str(count) + "개")
